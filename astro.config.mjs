0713207d07bc7b4932aa5f17f2f970e8cf0c5713
teddy.bear <teddy.bear@lgvoltup.com>
2026-04-03 20:06:46 +0900

    chore: cname 지정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import tailwind from '@astrojs/tailwind';
 
 // GitHub Pages 프로젝트 사이트: https://<user>.github.io/<repo>/
 export default defineConfig({
-  site: 'https://gongdongho12.github.io',
-  base: '/teddy-resume',
+  site: 'https://resume.dongholab.com',
+  base: '/',
   output: 'static',
   integrations: [tailwind({ applyBaseStyles: true })],
 });

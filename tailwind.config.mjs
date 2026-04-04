5995d28401b252a22ff4a2e1128e3cd1f0812842
teddy.bear <teddy.bear@lgvoltup.com>
2026-04-04 11:41:44 +0900

    chore: A4 사이즈 로직 추가

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,16 @@ export default {
   content: ['./src/**/*.{astro,html,js,ts,jsx,tsx,md,mdx}'],
   theme: {
     extend: {
+      spacing: {
+        'a4-w': '210mm',
+        'a4-h': '297mm',
+      },
+      width: {
+        'a4': '210mm',
+      },
+      height: {
+        'a4': '297mm',
+      },
       fontFamily: {
         sans: [
           '"Noto Sans KR"',
